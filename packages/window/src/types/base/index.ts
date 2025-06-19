e4cc7391956d41e05fca4b89167254afa66cf239
邢卓阳 <xzy2020910@bupt.edu.cn>
2025-06-19 16:45:00 +0800

    feat: define window meta

--- packages/window/src/types/base/index.ts
+++ packages/window/src/types/base/index.ts
@@ -4,3 +4,12 @@
 export type Position<T extends "2D" | "3D"> = T extends "3D"
 	? { x: number; y: number; z: number }
 	: { x: number; y: number }
+
+/**
+ *
+ * @description 3D size in space, compatible 2D by ignoring the z-axis.
+ * @example
+ */
+export type Size<T extends "2D" | "3D"> = T extends "3D"
+	? { width: number; height: number; depth: number }
+	: { width: number; height: number }
--- packages/window/src/types/window/index.ts
+++ packages/window/src/types/window/index.ts
@@ -1,8 +1,18 @@
-import { Position } from "../base"
+import { Position, Size } from "../base/index.js"
 
 export type WindowMeta = {
 	id: string
-	name: string
-	title: string
-	position: Position<"2D">
+	name?: string
+	title?: string
+	position?: Position<"2D">
+	resize?: Size<"2D">
+	maximized?: boolean
+	minimized?: boolean
+	focused?: boolean
+	visible?: boolean
+	header?: boolean
+	resizable?: boolean
+	draggable?: boolean
+	closable?: boolean
+	fullscreen?: boolean
 }
